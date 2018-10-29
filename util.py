0fcdca43e780ed52c6b6d1af253cb207780bf03c
guidojw <35309288+guidojw@users.noreply.github.com>
2018-10-29 14:47:44 +0100

    Changed scope.

--- util.py
+++ util.py
@@ -1,37 +1,37 @@
 import json
-
-Tweets = []
 	
 ### output: array
 def getTweets():
+	tweets = []
 	with open("p2000a.txt", "r") as f:
 		lines = f.readlines()
 		for line in lines:
-			Tweets.append(json.loads(line))
+			tweets.append(json.loads(line))
 	with open("p2000b.txt", "r") as f:
 		lines = f.readlines()
 		for line in lines:
-			Tweets.append(json.loads(line))
+			tweets.append(json.loads(line))
+	return tweets
 
 ### input: array keys, string val
 ### output: array 
-def getTweetsByAttributes(keys, val):
-	tweets = []
-	for attribute in Tweets:
+def getTweetsByAttributes(tweets, keys, val):
+	filtered = []
+	for attribute in tweets:
 		original = attribute;
 		for key in keys:
 			attribute = attribute[key]
 			if not attribute:
 				break
 		if attribute.lower() == val.lower():
-			tweets.append(original)
-	return tweets
+			filtered.append(original)
+	return filtered
 
 ### input: array keys
 ### output: array 
-def getUniqueValues(keys):
+def getUniqueValues(tweets, keys):
 	values = []
-	for attribute in Tweets:
+	for attribute in tweets:
 		original = attribute;
 		for key in keys:
 			attribute = attribute[key]
@@ -40,5 +40,3 @@ def getUniqueValues(keys):
 		values.append(attribute)
 	values = set(values)
 	return values
-
-getTweets()
