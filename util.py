9e37dde6bd45855df889cec843d7bf91e2a49e43
guidojw <35309288+guidojw@users.noreply.github.com>
2018-10-29 14:15:57 +0100

    Util module
    - Functionality to filter tweets by different indexes.
    - Functionality to get all unique values of tweets' indexes.

---
+++ util.py
@@ -0,0 +1,39 @@
+import json
+
+Tweets = []
+	
+def getTweets():
+	with open("p2000.txt", "r") as f:
+		lines = f.readlines()
+		for line in lines:
+			Tweets.append(json.loads(line))
+
+### input: array keys, string val
+### output: array 
+def getTweetsByAttributes(keys, val):
+	tweets = []
+	for attribute in Tweets:
+		original = attribute;
+		for key in keys:
+			attribute = attribute[key]
+			if not attribute:
+				break
+		if attribute.lower() == val.lower():
+			tweets.append(original)
+	return tweets
+
+### input: array keys
+### output: array 
+def getUniqueValues(keys):
+	values = []
+	for attribute in Tweets:
+		original = attribute;
+		for key in keys:
+			attribute = attribute[key]
+			if not attribute:
+				break
+		values.append(attribute)
+	values = set(values)
+	return values
+
+getTweets()
