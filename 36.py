cfef6fcc7edcbf6bd97a08f689581f007564431f
naimeds10 <44572522+naimeds10@users.noreply.github.com>
2018-10-30 12:33:18 +0100

    Add files via upload

--- 36.py
+++ 36.py
@@ -3,6 +3,8 @@ import random
 from eca.generators import start_offline_tweets
 import datetime
 import textwrap
+import pprint
+import re
 ## You might have to update the root path to point to the correct path
 ## (by default, it points to <rules>_static)
 root_content_path = '36_static'
@@ -12,9 +14,30 @@ root_content_path = '36_static'
 # the action will be called with the context and the event
 @event('init')
 def setup(ctx, e):
-    start_offline_tweets('p2000a.txt', time_factor=1000, event_name='chirp'); start_offline_tweets('p2000b.txt', time_factor=1000, event_name='chirp')
+    start_offline_tweets('p2000.txt', time_factor=150, event_name='chirp')
+    #start wordcloud
+    ctx.words = {}
+
+pattern = re.compile('\W+')
+
+stopwords = ['het', 'een', 'aan', 'zijn', 'http', 'www', 'com', 'ben', 'jij']
+
+def words(message):
+    result = pattern.split(message)
+    result = map(lambda w: w.lower(), result)
+    result = filter(lambda w: w not in stopwords, result)
+    result = filter(lambda w: len(w) > 2, result)
+    return result
+#end wordcloud
 
 @event('chirp')
 def tweet(ctx, e):
     tweet = e.data
     emit('tweet', tweet)
+#start wordcloud
+    for w in words(tweet['text']):
+        emit('wordcloud', {
+            'action': 'add',
+            'value': (w, 1)
+        })
+#end wordcloud
