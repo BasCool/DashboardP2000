93a3818e610d486cb47957170210ae7ca55919c0
naimeds10 <44572522+naimeds10@users.noreply.github.com>
2018-10-30 11:41:10 +0100

    Add files via upload

---
+++ 36.py
@@ -0,0 +1,20 @@
+from eca import *
+import random
+from eca.generators import start_offline_tweets
+import datetime
+import textwrap
+## You might have to update the root path to point to the correct path
+## (by default, it points to <rules>_static)
+root_content_path = '36_static'
+
+
+# binds the 'setup' function as the action for the 'init' event
+# the action will be called with the context and the event
+@event('init')
+def setup(ctx, e):
+    start_offline_tweets('p2000.txt', time_factor=1000, event_name='chirp')
+
+@event('chirp')
+def tweet(ctx, e):
+    tweet = e.data
+    emit('tweet', tweet)
